4adac997b74a555aa549db13c300b98c167d7925
Luca Braglia <lbraglia@gmail.com>
2014-12-26 00:00:00 +0000

    version 0.5

---
+++ R/aprean3-package.R
@@ -0,0 +1,16 @@
+#' Datasets from Draper and Smith "Applied Regression Analysis" (3rd ed.)
+#'
+#' This package is a unofficial companion to the textbook "Applied
+#' Regression Analysis" by N.R. Draper and H. Smith (3rd ed.).
+#'
+#' It includes almost all the published datasets, both for easy
+#' reproducibility and algorithms benchmarking purposes.
+#'
+#' Dataset names are prefixed by 'ds' and lowercased; furthermore '-' and
+#' '.' are replaced by '_'. Finally variables have lower-case name. 
+#'
+#' @name aprean3
+#' @docType package
+#' @source Draper, N.R., Smith, H. (1998) Applied Regression Analysis, 3rd
+#' ed., New York: Wiley
+NULL
